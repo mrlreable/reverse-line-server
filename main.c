a77d28e3e2e96fee8acd1b2664f91f3c2bd9cc69
mrlreable <71119678+mrlreable@users.noreply.github.com>
2021-02-26 15:09:15 +0100

    organized the reversing line algorithm into a separate function

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    long int lineSize = 1000;
-    char line[lineSize];
-    char reversedLine[lineSize];
-    printf("Enter a line: ");
-    fgets(line, lineSize, stdin);
+const long int lineSize = 1000;
 
+void reverseLine(char line[]){
+    char reversedLine[lineSize];
     int count, i = 0;
     while(line[count] != '\0'){
         count++;
@@ -19,6 +15,15 @@ int main()
     }
     reversedLine[i] = '\0';
     printf("%s", reversedLine);
+}
+
+int main()
+{
+    char line[lineSize];
+    printf("Enter a line: ");
+    fgets(line, lineSize, stdin);
+
+    reverseLine(line);
 
     return 0;
 }
