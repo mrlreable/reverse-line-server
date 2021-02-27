0ef00d3a8d4b6ea880f8eb03dc1d02fb9645ec45
mrlreable <71119678+mrlreable@users.noreply.github.com>
2021-02-27 21:09:46 +0100

    created server side and the function for chat between server and client in main.c

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <winsock2.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX 1000
+#define PORT 8080
+#define SA struct sockaddr
+#pragma comment(lib,"libws2_32.a")//Winsock Library
 
-const long int lineSize = 1000;
+//build: gcc main.c -o main.exe -lws2_32
+//run: main.c
 
 void reverseLine(char line[]){
-    char reversedLine[lineSize];
+    char reversedLine[MAX];
     int count, i = 0;
     while(line[count] != '\0'){
         count++;
@@ -16,14 +23,55 @@ void reverseLine(char line[]){
     reversedLine[i] = '\0';
     printf("%s", reversedLine);
 }
+void initWinSock(){
+    WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
 
+	}
+
+	printf("Initialized.");
+}
+void func(int sockfd){
+    char buff[MAX];
+    int n;
+    // infinite loop for chat
+    for (;;) {
+        bzero(buff, MAX);
+
+        // read the message from client and copy it in buffer
+        read(sockfd, buff, sizeof(buff));
+        // print buffer which contains the client contents
+        printf("From client: %s\t To client : ", buff);
+        bzero(buff, MAX);
+        n = 0;
+        // copy server message in the buffer
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+
+        // and send that buffer to client
+        write(sockfd, buff, sizeof(buff));
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
+}
 int main()
 {
-    char line[lineSize];
+    char line[MAX];
     printf("Enter a line: ");
-    fgets(line, lineSize, stdin);
+    fgets(line, MAX, stdin);
+
+    //initializing winsock
+    initWinSock();
 
     reverseLine(line);
+
+
 
     return 0;
 }
